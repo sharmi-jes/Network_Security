0d1808f1466aa9bc5fe69d40f97d03159d7a1f43
sharmi-jes <anyumsgarmila@gmail.com>
2025-02-19 20:45:12 +0530

    added mlflow server

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pandas as pd
+import mlflow
 
 from networksecurity.exception import NetworkSecurityException
 from networksecurity.logging import logging
@@ -38,6 +39,21 @@ class ModelTrainer:
             return pd.read_csv(file_path)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision_score",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+
+            mlflow.sklearn.log_model(best_model,"model")
+
+
         
     def train_model(self,x_train,y_train,x_test,y_test):
         try:
@@ -96,11 +112,15 @@ class ModelTrainer:
 
             # clasculate the alls cores
 
-            classification_train_score=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+            classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+
+            self.track_mlflow(best_model,classification_train_metric)
 
             y_test_pred=best_model.predict(x_test)
 
-            classification_test_score=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+            classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+
+            self.track_mlflow(best_model,classification_test_metric)
 
             # take the preprocessor objetc
             preprocessor=load_object(self.data_transformation_artifact.transformed_object_file_path)
